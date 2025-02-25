46e007283ac6c9b273cba0f22cb0e1e5aa0476d0
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2025-02-25 10:27:51 +0300

    work

--- root_output.py
+++ root_output.py
@@ -2,4 +2,4 @@ import root_application as rp
 
 radius = 7
 area = rp.area_of_the_circle(radius)
-print(f'The area of the circle with radius {radius} is : {area:.2f}')
+print(f'The area of the circle with radius {radius} is : {area:.2f}')  # This prints the output.
